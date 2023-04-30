e68f0a7ea4741ce41fd3d123368ea429c58a634d
unknown <tuan.lenguyen@di4l.vn>
2023-04-30 17:30:27 +0700

    finish order

--- orders/controller/orders.controller.ts
+++ orders/controller/orders.controller.ts
@@ -2,7 +2,7 @@ import "express-async-errors";
 import { Request, Response } from "express";
 import * as Order from "../services/order.services";
 import { validationResult } from "express-validator";
-import { RequestValidationError } from "@microservies-inventory/common/build";
+import { RequestValidationError } from "@microservies-inventory/common";
 
 export const viewAllOrders = async (req: Request, res: Response) => {
   const listOrders = await Order.viewAllOrders();
@@ -32,17 +32,25 @@ export const checkOrder = async (req: Request, res: Response) => {
 
   type Product = {
     product_name: string;
+    category: string;
     bar_code: string;
-    quantity: number;
     sku: string;
+    unit: string;
+    quantity: number;
     supplier_name: string;
   };
 
-  const detailOrder = await Order.viewDetailOrders(req.params.id);
+  type Package = {
+    package_code: string,
+    products: Array<Product>
+  }
+
+  const detailOrder = await Order.findOneOrder(req.params.id);
   const checkProduct = req.body.products;
 
-  let missingProducts = [];
-  detailOrder!.packages.forEach((packages) => {
+  let missingProducts: Array<Package> = [];
+
+  missingProducts = detailOrder!.packages.map((packages) => {
     let listProductMissing: Array<Product> = packages.products.filter(
       (product) => {
         const matchingCheckProduct = checkProduct.find(
@@ -57,55 +65,43 @@ export const checkOrder = async (req: Request, res: Response) => {
           const quantityDifference =
             product.quantity - matchingCheckProduct.quantity;
           if (quantityDifference > 0) {
-            console.log(2);
+            product.quantity = quantityDifference;
+            return product;
           }
         } else {
           return product;
         }
       }
     );
-  });
 
-  // console.log("\n       -----------------           \n", missingProducts);
+    if (listProductMissing.length > 0) {
+      return {
+        package_code: packages.package_code,
+        products: listProductMissing,
+      };
+    } else {
+      return null;
+    }
+  }).filter((missingPackage) => missingPackage !== null) as Package[];
 
-  // let missingProducts = detailOrder!.packages.filter((packages) => {
-  //   // check tung phan tu cua detail package
-  //   const packageCode = packages.package_code;
-  //   let missingProductsForPackage = products.filter(
-  //     (productToCheck: Product) => {
-  //       let found: boolean = false;
-  //       packages.products.filter((product) => {
-  //         if (
-  //           productToCheck.product_name === product.product_name &&
-  //           productToCheck.bar_code === product.bar_code &&
-  //           productToCheck.sku === product.sku &&
-  //           productToCheck.supplier_name === product.supplier_name
-  //         ) {
-  //           if (product.quantity >= productToCheck.quantity) {
-  //             found = true;
-  //           }
-  //           return;
-  //         }
-  //       });
-  //       if (!found) {
-  //         return { products: productToCheck };
-  //       }
-  //     }
-  //   );
+  if (missingProducts.length === 0) {
+    let order = await Order.findOneAndUpdate(req.params.id, { status: "Stocked" });
 
-  //   if (missingProductsForPackage.length > 0) {
-  //     return { packageCode: packageCode, products: products };
-  //   }
-  // });
-  // const result = {
-  //   missing: missingProducts,
-  // };
-
-  // console.log(result);
-
-  res.status(200).send({ product: req.body.products });
-
-  // const result = {
-  //   missing: missingProducts,
-  // };
+    res.status(200).send({ status: "The Order is Stocked Finished" });
+  } else {
+    Order.findOneAndUpdate(req.params.id, { status: "Not Enough Stock" }).then((result) => {
+      Order.insertOrder({
+        container_code: result?.container_code || "",
+        deliverer: result?.deliverer || "",
+        license_plates: result?.license_plates || "",
+        store_keeper: result?.store_keeper || "",
+        order_type: "Warehouse Orders Repack",
+        status: "Unchecked",
+        packages: missingProducts,
+        stack_car: result?.stack_car || false,
+        parentID: result?._id
+      });
+      res.status(200).send({ status: "The Order not finished Because Not Enough Stock", missingProducts: missingProducts });
+    });
+  }
 };
--- orders/services/order.services.ts
+++ orders/services/order.services.ts
@@ -1,3 +1,4 @@
+import { BadRequestError } from "@microservies-inventory/common";
 import { Order } from "../models/order.model";
 
 export const viewAllOrders = async () => {
@@ -11,7 +12,22 @@ export const insertOrder = async (order_attributes: Order) => {
   return order.save();
 };
 
-export const viewDetailOrders = async (id: string) => {
+export const findOneOrder = async (id: string) => {
   const order = await Order.findById(id);
+
+  if (!order) {
+    throw new BadRequestError(`Order not found: ${id}`);
+  }
+
+  return order;
+};
+
+export const findOneAndUpdate = async (id: string, dataObj: Record<string, unknown>) => {
+  const order = await Order.findOneAndUpdate({ _id: id }, dataObj);
+
+  if (order?.parentID !== null && order?.status === "Stocked") {
+    await findOneAndUpdate(String(order?.parentID), { status: "Stocked" });
+  }
+
   return order;
 };
