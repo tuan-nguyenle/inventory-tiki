899b3f004fa03113a7ac86d4a8a98be0458542cc
unknown <tuan.lenguyen@di4l.vn>
2023-05-10 17:22:21 +0700

    Updates

---
+++ common/src/event/rabbitmq.ts
@@ -0,0 +1,48 @@
+import amqp from 'amqplib';
+import { Subjects } from './subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class RabbitMQ<T extends Event> {
+  protected connection!: amqp.Connection;
+  protected channel!: amqp.Channel;
+  protected exchangeName: string;
+  protected exchangeType: string;
+  abstract queueName: T['subject'];
+  protected routingKey: string;
+
+  constructor(
+    exchangeName: string,
+    exchangeType: string,
+    routingKey: string,
+  ) {
+    this.exchangeName = exchangeName;
+    this.exchangeType = exchangeType;
+    this.routingKey = routingKey;
+  }
+
+  async connect(): Promise<void> {
+    this.connection = await amqp.connect('amqp://localhost:5673');
+    this.channel = await this.connection.createChannel();
+
+    await this.channel.assertExchange(this.exchangeName, this.exchangeType);
+    await this.channel.assertQueue(this.queueName);
+  }
+
+  async sendMessage(message: T['data']): Promise<void> {
+    await this.channel.publish(this.exchangeName, this.routingKey, Buffer.from(JSON.stringify(message)));
+  }
+
+  async consumeMessage(): Promise<void> {
+    await this.channel.bindQueue(this.queueName, this.exchangeName, this.routingKey);
+    await this.channel.consume(this.queueName, (msg) => {
+      if (msg !== null) {
+        console.log(msg.content.toString());
+        // this.channel.ack(msg);
+      }
+    }, { noAck: true });
+  };
+}
--- common/src/index.ts
+++ common/src/index.ts
@@ -2,3 +2,7 @@ export * from "./middleware/error-handler";
 export * from "./middleware/authencation";
 export * from "./middleware/error/error-messages";
 export * from "./middleware/error/errors";
+
+export * from "./event/interface/InsertProductToPalletPublisher";
+export * from "./event/rabbitmq";
+export * from "./event/subjects";
