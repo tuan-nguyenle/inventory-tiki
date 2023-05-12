ab0c07c9c29bda0f08b3306e6e40f85a995872ea
unknown <tuan.lenguyen@di4l.vn>
2023-05-13 01:50:47 +0700

    Updates

--- common/src/event/subjects.ts
+++ common/src/event/subjects.ts
@@ -1,5 +1,6 @@
 export enum Subjects {
   RequestInsertedProductToPallet = "pallet:inserted",
-  // OrdersCreated = "orders:created",
+  TransferToShelfFromPallet = "pallet:updated",
+  ProductCreated = "Product:created",
   // OrdersUpdated = "orders:updated",
 }
