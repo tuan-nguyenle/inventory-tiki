0a0a6b57b4ef980b82e9edcaf677711df8126e4b
unknown <tuan.lenguyen@di4l.vn>
2023-05-13 02:18:34 +0700

    Updates

--- common/src/event/interface/PalletUpdated.ts
+++ common/src/event/interface/PalletUpdated.ts
@@ -3,6 +3,7 @@ import { Subjects } from "../subjects";
 export interface PalletUpdated {
     subject: Subjects.PalletUpdated;
     data: {
+        name_pallet: string,
         product: {
             product_name: string,
             category: string,
