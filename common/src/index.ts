99e0411d7915849d3b5cf241097883dee5f4c1a1
unknown <tuan.lenguyen@di4l.vn>
2023-05-13 02:52:56 +0700

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -5,5 +5,6 @@ export * from "./middleware/error/errors";
 
 export * from "./event/interface/InsertProductToPalletPublisher";
 export * from "./event/interface/PalletUpdated";
+export * from "./event/interface/ProductCreated";
 export * from "./event/rabbitmq";
 export * from "./event/subjects";
