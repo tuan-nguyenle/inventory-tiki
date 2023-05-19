542e2fadb99080c157c13bb0f6c8bbe6167685a0
unknown <tuan.lenguyen@di4l.vn>
2023-05-19 09:22:04 +0700

    add middleware

--- warehouse/routers/warehouse.router.ts
+++ warehouse/routers/warehouse.router.ts
@@ -3,15 +3,16 @@ import express from "express";
 import * as PalletController from "../controller/pallet.controller";
 // import { body } from "express-validator";
 import * as ShelfController from "../controller/shelf.controller";
+import { requireAuthor } from "@microservies-inventory/common";
 const router = express.Router();
 
 // get list api
-router.post("/api/warehouse/pallets", PalletController.insertPallet);
-router.get("/api/warehouse/pallets", PalletController.searchPallet);
-router.post("/api/warehouse/pallets/:id", PalletController.updateStatus);
+router.post("/api/warehouse/pallets", requireAuthor, PalletController.insertPallet);
+router.get("/api/warehouse/pallets", requireAuthor, PalletController.searchPallet);
+router.post("/api/warehouse/pallets/:id", requireAuthor, PalletController.updateStatus);
 
 // shelf
-router.post("/api/warehouse/shelf", ShelfController.transferToShelf);
-router.get("/api/warehouse/shelf", ShelfController.findShelf);
+router.post("/api/warehouse/shelf", requireAuthor, ShelfController.transferToShelf);
+router.get("/api/warehouse/shelf", requireAuthor, ShelfController.findShelf);
 
 export { router as warehouse };
