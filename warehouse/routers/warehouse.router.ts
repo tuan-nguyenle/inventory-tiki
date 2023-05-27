91491544f0a5ab34f8cf4561d4bd8993dbf26340
unknown <tuan.lenguyen@di4l.vn>
2023-05-27 14:36:34 +0700

    e

--- warehouse/routers/warehouse.router.ts
+++ warehouse/routers/warehouse.router.ts
@@ -1,9 +1,10 @@
 import "express-async-errors";
-import express from "express";
+import express, { Request, Response } from "express";
 import * as PalletController from "../controller/pallet.controller";
 // import { body } from "express-validator";
 import * as ShelfController from "../controller/shelf.controller";
 import { requireAuthor, userAuthor } from "@microservies-inventory/common";
+import { Shelf } from "../models/shelf.model";
 const router = express.Router();
 
 // get list api
@@ -15,5 +16,25 @@ router.post("/api/warehouse/pallets/:id", userAuthor, requireAuthor, PalletContr
 router.post("/api/warehouse/shelf", userAuthor, requireAuthor, ShelfController.transferToShelf);
 router.get("/api/warehouse/shelf", userAuthor, requireAuthor, ShelfController.findShelf);
 router.post("/api/warehouse/findMultipleShelf", userAuthor, requireAuthor, ShelfController.findMultipleShelf);
+router.post("/api/warehouse/findShelfSpecific", userAuthor, requireAuthor, async (req: Request, res: Response) => {
+    const listShelf = await Promise.all(
+        req.body.products.map(async (product: any) => {
+
+            const shelf = await Shelf.findOne(
+                {
+                    "products.product_name": product.product_name,
+                    "products.supplier_name": product.supplier_name,
+                    "products.bar_code": product.bar_code,
+                    "products.sku": product.sku
+                }
+            );
+            return { product_name: product.product_name, shelves: shelf };
+        })
+    );
+
+    res.status(200).send({
+        msg: listShelf
+    });
+});
 
 export { router as warehouse };
